0e24df0ce74c6f666bff63a48c0b291b3aa1ff3b
jjny <jminfir@gmail.com>
2016-06-14 10:29:43 +0200

    page admin
    construction page admin
    utilisation assetic

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -32,6 +32,7 @@ class ComposerStaticInit5b9d1cffa82704c6a96e56bbdc329bbd
             'Symfony\\Component\\' => 18,
             'Symfony\\Bundle\\SwiftmailerBundle\\' => 33,
             'Symfony\\Bundle\\MonologBundle\\' => 29,
+            'Symfony\\Bundle\\AsseticBundle\\' => 29,
             'Symfony\\Bundle\\' => 15,
             'Symfony\\Bridge\\Twig\\' => 20,
             'Symfony\\Bridge\\Swiftmailer\\' => 27,
@@ -102,6 +103,10 @@ class ComposerStaticInit5b9d1cffa82704c6a96e56bbdc329bbd
         'Symfony\\Bundle\\MonologBundle\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/monolog-bundle',
+        ),
+        'Symfony\\Bundle\\AsseticBundle\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/assetic-bundle',
         ),
         'Symfony\\Bundle\\' => 
         array (
