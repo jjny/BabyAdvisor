28ef5f95d5d445cd6eb33f845f96950a5a636a50
Dimitri <dimi_didou@hotmail.fr>
2016-06-16 21:36:39 +0200

    Update

--- vendor/bin/update-sql-reserved-words.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../mysql-workbench-schema-exporter/mysql-workbench-schema-exporter/bin/update-sql-reserved-words
-php "%BIN_TARGET%" %*
